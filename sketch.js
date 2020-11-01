d5d2661f05138edc14287ccbad0c3c726f213361
SanaNotUwU <73371481+SanaNotUwU@users.noreply.github.com>
2020-11-01 14:24:23 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,30 @@
-var movingRect, fixedRect
+var fixedRect, movingRect;
+
 function setup() {
-  createCanvas(800,400);
-
-  fixedRect = createSprite(400, 200, 50, 50);
-  fixedRect.shapeColor = "green"
- fixedRect.debug = true;
-
-  movingRect = createSprite(400,600,60,70)
-  movingRect.shapeColor = "blue"
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
   movingRect.debug = true;
 
-  movingRect.velocityY = -5
-  fixedRect.velocityY = +5
-  
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(255,255,255);  
- //movingRect.x = mouseX
-  //movingRect.y = mouseY
-  if (movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2 && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 ){
-  fixedRect.velocityX = fixedRect.velocityX *(-1)
-  movingRect.velocityX = movingRect.velocityX *(-1)
+  background(0,0,0);  
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
   }
-  if  (movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2 && movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2){
-  fixedRect.velocityY = fixedRect.velocityY *(-1)
-  movingRect.velocityY = movingRect.velocityY *(-1)
+  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
   }
   drawSprites();
 }
